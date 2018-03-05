97cf0c6ce72ecb510f34f2233f026da1fc4a729b
bettybonilla <bettybonillap@gmail.com>
2018-03-05 16:05:14 -0600

    Initial commit

---
+++ app/src/main/java/com/example/beatr/ventanafiscal/SplashActivity.java
@@ -0,0 +1,24 @@
+package com.example.beatr.ventanafiscal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() { //para cambiar a la siguiente actividad despues de un tiempo
+            @Override
+            public void run() {
+                Intent it =new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(it);
+                finish();
+            }
+        },4000 );
+    }
+
+}
