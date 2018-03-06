7404b56d38d55ce6fe5b37d471af50ae5d51f206
bettybonilla <bettybonillap@gmail.com>
2018-03-06 17:00:12 -0600

    Whatsapp agregado

--- app/src/main/java/com/example/beatr/ventanafiscal/NosotrosFragment.java
+++ app/src/main/java/com/example/beatr/ventanafiscal/NosotrosFragment.java
@@ -2,11 +2,14 @@ package com.example.beatr.ventanafiscal;
 
 
 import android.app.Fragment;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
+import android.telephony.PhoneNumberUtils;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
 import android.text.style.StyleSpan;
@@ -16,6 +19,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -23,7 +27,7 @@ import android.widget.TextView;
  */
 public class NosotrosFragment extends Fragment {
     private TextView textView;
-    private ImageButton imageButton;
+    private ImageButton imageButton, imageButton2;
 
     public NosotrosFragment() {
         // Required empty public constructor
@@ -42,13 +46,14 @@ public class NosotrosFragment extends Fragment {
     }
     final SpannableStringBuilder sb = new SpannableStringBuilder("\n\n\n\n\n\n\n\n\n" +
             "Sobre nosotros\n\n\n\n\n\n" +
-            "Somos un despacho contable.");
+            "Somos un despacho contable que atiende personas físical y morales. Somos completamente honestos y apegados a las leyes fiscales." +
+            "\n\n\nPara cualquier asesoría o informes no dude en contactarnos.");
     final StyleSpan bss = new StyleSpan(android.graphics.Typeface.BOLD);
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         //imageView=(ImageView) view.findViewById(R.id.imageView2);
         textView=(TextView) view.findViewById(R.id.nosotros);
-        imageButton=(ImageButton)view.findViewById(R.id.imageButton);
+        imageButton=(ImageButton)view.findViewById(R.id.imageButtonC);
         sb.setSpan(bss, 10, 23, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         textView.setText(sb);
         imageButton.setOnClickListener(new View.OnClickListener() {
@@ -59,6 +64,42 @@ public class NosotrosFragment extends Fragment {
                 getActivity().startActivity(intent);
             }
         });
+        imageButton2=(ImageButton)view.findViewById(R.id.imageButtonW);
+        imageButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openWhatsApp();
+            }
+        });
+
     }
 
+    private void openWhatsApp() {
+        String smsNumber = "+5215585754039";
+        boolean isWhatsappInstalled = whatsappInstalledOrNot("com.whatsapp");
+        if (isWhatsappInstalled) {
+
+            Intent sendIntent = new Intent("android.intent.action.MAIN");
+            sendIntent.setComponent(new ComponentName("com.whatsapp", "com.whatsapp.Conversation"));
+            sendIntent.putExtra("jid", PhoneNumberUtils.stripSeparators(smsNumber) + "@s.whatsapp.net");//phone number without "+" prefix
+
+            startActivity(sendIntent);
+        } else {
+            Uri uri = Uri.parse("market://details?id=com.whatsapp");
+            Intent goToMarket = new Intent(Intent.ACTION_VIEW, uri);
+            Toast.makeText(getActivity(),"WhatsApp not Installed",Toast.LENGTH_SHORT).show();
+            getActivity().startActivity(goToMarket);
+        }
+    }
+    private boolean whatsappInstalledOrNot(String uri) {
+        PackageManager pm = getActivity().getPackageManager();
+        boolean app_installed = false;
+        try {
+            pm.getPackageInfo(uri, PackageManager.GET_ACTIVITIES);
+            app_installed = true;
+        } catch (PackageManager.NameNotFoundException e) {
+            app_installed = false;
+        }
+        return app_installed;
+    }
 }
