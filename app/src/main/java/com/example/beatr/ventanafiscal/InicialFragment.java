94b3c412e28bdd02e9839217dc9e219062b6b810
bettybonilla <bettybonillap@gmail.com>
2018-03-06 16:07:55 -0600

    Nosotros agregado

--- app/src/main/java/com/example/beatr/ventanafiscal/InicialFragment.java
+++ app/src/main/java/com/example/beatr/ventanafiscal/InicialFragment.java
@@ -4,6 +4,9 @@ package com.example.beatr.ventanafiscal;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 //import android.support.v4.app.Fragment;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +18,7 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  */
 public class InicialFragment extends Fragment {
-
+    private TextView textView;
 
     public InicialFragment() {
         // Required empty public constructor
@@ -32,11 +35,14 @@ public class InicialFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_inicial, container, false);
     }
-
+    final SpannableStringBuilder sb = new SpannableStringBuilder("Bienvenido a Ventana Fiscal\n\n\nEsta aplicación tiene el objetivo de ayudarte a cumplir con tus obligaciones fiscales.");
+    final StyleSpan bss = new StyleSpan(android.graphics.Typeface.BOLD);
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         //imageView=(ImageView) view.findViewById(R.id.imageView2);
-        //textView=(TextView) view.findViewById(R.id.textView);
+        textView=(TextView) view.findViewById(R.id.inicial);
+        sb.setSpan(bss, 0, 26, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        textView.setText(sb);
     }
 
 }
--- app/src/main/java/com/example/beatr/ventanafiscal/NosotrosFragment.java
+++ app/src/main/java/com/example/beatr/ventanafiscal/NosotrosFragment.java
@@ -2,19 +2,28 @@ package com.example.beatr.ventanafiscal;
 
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
+import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class NosotrosFragment extends Fragment {
-
+    private TextView textView;
+    private ImageButton imageButton;
 
     public NosotrosFragment() {
         // Required empty public constructor
@@ -31,10 +40,25 @@ public class NosotrosFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_nosotros, container, false);
     }
-
+    final SpannableStringBuilder sb = new SpannableStringBuilder("\n\n\n\n\n\n\n\n\n" +
+            "Sobre nosotros\n\n\n\n\n\n" +
+            "Somos un despacho contable.");
+    final StyleSpan bss = new StyleSpan(android.graphics.Typeface.BOLD);
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         //imageView=(ImageView) view.findViewById(R.id.imageView2);
-        //textView=(TextView) view.findViewById(R.id.textView);
+        textView=(TextView) view.findViewById(R.id.nosotros);
+        imageButton=(ImageButton)view.findViewById(R.id.imageButton);
+        sb.setSpan(bss, 10, 23, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        textView.setText(sb);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:5585754039"));
+                getActivity().startActivity(intent);
+            }
+        });
     }
+
 }
